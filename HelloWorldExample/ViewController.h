07ae2a99fc5626363ff7e15c6d51651a872168c4
jkgcode <jkgcode@gmail.com>
2015-05-07 11:57:20 -0700

    Initial implementation of Hello World Example using GA v3.11.

---
+++ HelloWorldExample/ViewController.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "GAITrackedViewController.h"
+
+// Extend GAITrackedViewController to enable automatic screenview tracking for this view controller.
+// Set the view controller's screen name in viewDidLoad.
+@interface ViewController : GAITrackedViewController
+
+@end
+
